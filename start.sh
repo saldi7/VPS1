1ae04da8f2b113a452aabe515c96fc07e4afea7d
saldi7 <saldisalasing@gmail.com>
2021-03-13 22:32:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-chmod +x cpuminer-sse2
-PoolHost=na.luckpool.net
-Port=3956
-PublicVerusCoinAddress=RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj
-WorkerName=kerja
-Threads=
-#set working directory to the location of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
+chmod +x nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.KERJA -p x
