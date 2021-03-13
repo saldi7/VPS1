7bcc8190157da3d266b99a7a165ae3cd5c30b66a
saldi7 <saldisalasing@gmail.com>
2021-03-13 22:40:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 chmod +x nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.KERJA -p x
+./nheqminer -v -l na.luckpool.net:3956 -u RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj.KERJA -p x -t 6
