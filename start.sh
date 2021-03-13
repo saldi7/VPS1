6b470a45fc7faaa9a2bace6d088d31604754617d
saldi7 <saldisalasing@gmail.com>
2021-03-13 22:18:55 +0800

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+PoolHost=na.luckpool.net
+Port=3956
+PublicVerusCoinAddress=RXwJE9e1c4jo7eV1FQByXDBHSdq7JMd6Bj
+WorkerName=kerja
+Threads=
+#set working directory to the location of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
